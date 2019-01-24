4eb569effe2bee3a83984132c4c6f1f285c3fcde
RealOrko <45273739+RealOrko@users.noreply.github.com>
2019-01-24 19:06:51 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# dotnet-cli-zip
+
+A zipping and unzipping tool for the dotnet cli that works on osx-x64/linux-x64/win-x64 runtimes.
+
+## Why do I need this?
+
+You find yourself flick flacking when it comes to zipping and unzipping in build scripts on multiple runtimes. 
+On Windows you use powershell(Compress-Archive/Expand-Archive) but on Unix you use bash(zip/unzip) with nuances. 
+
+Please follow this <a target="dotnet_cli_zip_jump_target" href="https://github.com/dotnet/cli/issues/10675">issue</a>.
+
+## Install using the dotnet cli
+
+```
+dotnet tool install --global dotnet-cli-zip
+```
+
+## Using it
+
+All tested with `installing from source`(GitHub/Manually factorial RID visa vi [Powershell|Bash|Cmd]) could not verify `dotnet cli install` though, working on it! :)
+
+```
+z -e MyZip.zip ./MyZipFolder
+z -c ./MyZipFolder MyZip.zip
+```
+
+## Uninstalling
+
+If you want to uninstall
+
+```
+dotnet tool uninstall -g dotnet-cli-zip
+```
---
+++ src/zip/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace zip
+{
+    class Program
+    {
+        const int ActionOrdinal = 0;
+        const int FromOrdinal = 1;
+        const int ToOrdinal = 2;
+
+        const string ExtractAction = "-e";
+        const string CompressAction = "-c";
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0 || args[0] == "--help" || args[0] == "/?" || args[0] == "?" || args.Length != 3)
+            {
+                PrintUsage();
+                return;
+            }
+
+            try
+            {
+                var actionValue = args[ActionOrdinal];
+                var fromValue = args[FromOrdinal];
+                var toValue = args[ToOrdinal];
+
+                if (actionValue == ExtractAction)
+                {
+                    ExtractFilesRecursively(fromValue, toValue);
+                }
+                else if (actionValue == CompressAction)
+                {
+                    CompressFilesRecursively(fromValue, toValue);
+                }
+                else
+                {
+                    PrintUsage();
+                }
+            }
+            catch (Exception err)
+            {
+                PrintUsage();
+                Console.Error.WriteLine(err);
+            }
+        }
+
+        private static void CompressFilesRecursively(string folderPath, string zipPath)
+        {
+            Console.WriteLine($"Compressing from '{folderPath}' to '{zipPath}' recursively.");
+            ZipFile.CreateFromDirectory(folderPath, zipPath);
+        }
+
+        private static void ExtractFilesRecursively(string zipPath, string folderPath)
+        {
+            Console.WriteLine($"Extracting from '{folderPath}' to '{zipPath}' recursively.");
+            ZipFile.ExtractToDirectory(zipPath, folderPath, overwriteFiles:true);
+        }
+
+        static void PrintUsage()
+        {
+            Console.WriteLine("dotnet cli zip");
+            Console.WriteLine("---------------");
+            Console.WriteLine("Usage:");
+            Console.WriteLine("   > dotnet zip -e MyZip.zip ./MyZipFolder");
+            Console.WriteLine("   > dotnet zip -c ./MyZipFolder MyZip.zip");
+        }
+    }
+}
